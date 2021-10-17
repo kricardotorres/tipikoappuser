a81b781677fcffbe8ee10d88a85d97de219a6bfe
ricardo medina <ricardomedin@outlook.com>
2021-10-16 19:11:21 -0500

    ready implementation of template

--- lib/views/json_restful_api.dart
+++ lib/views/json_restful_api.dart
@@ -7,13 +7,16 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:tipiko_app_usr/data/json_user.dart';
 
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tipiko_app_usr/views/homepage.dart';
 import 'package:tipiko_app_usr/views/logged_screen.dart';
 
 const EdgeInsets pad20 = const EdgeInsets.all(20.0);
 const String spKey = 'myBool';
 const String key = 'user_authenticationToken';
 const String email = 'user_email';
+
 
 class LoginWithRestfulApi extends StatefulWidget {
 
@@ -110,9 +113,224 @@ class _LoginWithRestfulApiState extends State<LoginWithRestfulApi> {
 
   @override
   Widget build(BuildContext context) {
+    String defaultFontFamily = 'Roboto-Light.ttf';
+    double defaultFontSize = 14;
+    double defaultIconSize = 17;
 
-    {
-      return Scaffold(
+    {return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(left: 20, right: 20, top: 35, bottom: 30),
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white70,
+        child:
+        _isLoading
+            ? CircularProgressIndicator()
+            :
+        Column(
+          children: <Widget>[
+
+            Flexible(
+              flex: 8,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 230,
+                    height: 100,
+                    alignment: Alignment.center,
+                    child: Image.asset(
+                      "assets/images/menus/ic_food_express.png",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  TextField(
+                    controller: _emailController,
+                    showCursor: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      filled: true,
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Color(0xFF666666),
+                        size: defaultIconSize,
+                      ),
+                      fillColor: Color(0xFFF2F3F5),
+                      hintStyle: TextStyle(
+                          color: Color(0xFF666666),
+                          fontFamily: defaultFontFamily,
+                          fontSize: defaultFontSize),
+                      hintText: "Email",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  TextField(
+                    showCursor: true,
+                    obscureText: true,
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      filled: true,
+                      prefixIcon: Icon(
+                        Icons.lock_outline,
+                        color: Color(0xFF666666),
+                        size: defaultIconSize,
+                      ),
+                      suffixIcon: Icon(
+                        Icons.remove_red_eye,
+                        color: Color(0xFF666666),
+                        size: defaultIconSize,
+                      ),
+                      fillColor: Color(0xFFF2F3F5),
+                      hintStyle: TextStyle(
+                        color: Color(0xFF666666),
+                        fontFamily: defaultFontFamily,
+                        fontSize: defaultFontSize,
+                      ),
+                      hintText: "Contraseña",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    child: Text(
+                      "Forgot your password?",
+                      style: TextStyle(
+                        color: Color(0xFF666666),
+                        fontFamily: defaultFontFamily,
+                        fontSize: defaultFontSize,
+                        fontStyle: FontStyle.normal,
+                      ),
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+
+                  Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5.0)),
+      boxShadow: <BoxShadow>[
+        BoxShadow(
+          color: Color(0xFFfbab66),
+        ),
+        BoxShadow(
+          color: Color(0xFFf7418c),
+        ),
+      ],
+      gradient: LinearGradient(
+          colors: [Color(0xFFf7418c), Color(0xFFfbab66)],
+          begin: const FractionalOffset(0.2, 0.2),
+          end: const FractionalOffset(1.0, 1.0),
+          stops: [0.0, 1.0],
+          tileMode: TileMode.clamp),
+    ),
+    child: MaterialButton(
+    highlightColor: Colors.transparent,
+    splashColor: Color(0xFFf7418c),
+    //shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5.0))),
+    child: Padding(
+    padding:
+    const EdgeInsets.symmetric(vertical: 10.0, horizontal: 42.0),
+    child: Text(
+    "Entrar",
+    style: TextStyle(
+    color: Colors.white,
+    fontSize: 25.0,
+    fontFamily: "WorkSansBold"),
+    ),
+    ),
+    onPressed: () async {
+    setState(() => _isLoading = true);
+    var res = await _loginUser(
+    _emailController.text, _passwordController.text);
+    setState(() => _isLoading = false);
+
+    JsonUser user = JsonUser.fromJson(res);
+    if (user != null) {
+
+    _save(user);
+    Navigator.of(context).pushReplacement(MaterialPageRoute<Null>(
+    builder: (BuildContext context) {
+    return new HomePage(  );
+    }));
+    } else {
+    Scaffold.of(context).showSnackBar(
+    SnackBar(content: Text("Wrong email ")));
+    }
+    }),
+    ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                //FacebookGoogleLogin()
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                        "Don't have an account? ",
+                        style: TextStyle(
+                          color: Color(0xFF666666),
+                          fontFamily: defaultFontFamily,
+                          fontSize: defaultFontSize,
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () => {
+                       // Navigator.push(context, ScaleRoute(page: SignUpPage()))
+                      },
+                      child: Container(
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                            color: Color(0xFFf7418c),
+                            fontFamily: defaultFontFamily,
+                            fontSize: defaultFontSize,
+                            fontStyle: FontStyle.normal,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+      /*return Scaffold(
         key: _scaffoldKey,
         appBar: AppBar(title: Text("Iniciar sesion en Tipiko")),
         body: Center(
@@ -142,9 +360,8 @@ class _LoginWithRestfulApiState extends State<LoginWithRestfulApi> {
                 ),
               ),
 
-              RaisedButton(
+              ElevatedButton(
                 child: Text("Entrar"),
-                color: Colors.lightBlueAccent,
                 onPressed: () async {
                   setState(() => _isLoading = true);
                   var res = await _loginUser(
@@ -170,7 +387,7 @@ class _LoginWithRestfulApiState extends State<LoginWithRestfulApi> {
             ],
           ),
         ),
-      );
+      );*/
     }
 
 
